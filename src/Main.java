23757bb80abe2a041055f9930a3c7a73c9482013
Vu Minh Chien <vuminhchien2803@gmail.com>
2026-03-10 22:40:11 +0700

    chore: bai hoc ve bien(variables)

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Hello World");
-            System.out.println("Toi dang thuc hanh test CLI voi Git o day");
+            //khai báo tên cổ phiếu
+            String stockName = "VNM";
+
+            //Khai báo giá mua (số thực)
+            double buyPrice = 110.5;
+
+            //Khai báo số lượng mua (số nguyên)
+            int quantity = 100;
+
+            //Tính tổng tiền
+            double total = buyPrice * quantity;
+
+            System.out.println("Bạn vừa mua " + quantity + " cổ phiếu "+ stockName);
+            System.out.println("Tổng số tền là: " + total);
         }
     }
