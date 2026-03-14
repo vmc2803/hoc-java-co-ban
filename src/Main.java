0fbfd713b54380211b3619522e258fd326350413
Vu Minh Chien <vuminhchien2803@gmail.com>
2026-03-15 03:52:41 +0700

    chore: bai tap thuc hanh while loops + bolean -> sai chua fix

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,19 @@ import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        boolean tiepTuc = true;
-        while (tiepTuc){
+        boolean isLocked = true;
+        while (isLocked) {
+            System.out.print("Xin mời bạn nhập password: ");
             Scanner input = new Scanner(System.in);
+            String password = input.nextLine();
 
-            //nhập tên cổ phiếu
-            System.out.print("Nhập tên cổ phiếu: ");
-            String stockName = input.nextLine();
-
-            //Nhập giá mua vào
-            System.out.print("Nhập giá mua vào: ");
-            double buyPrice = Double.parseDouble(input.nextLine());
-
-            //nhập giá hiện tại
-            System.out.print("Nhập giá hiện tại: ");
-            double currentPrice = Double.parseDouble(input.nextLine());
-
-            //nhập số lượng sở hữu
-            System.out.print("Nhập số lượng: ");
-            int quantity = Integer.parseInt(input.nextLine());
-
-            double Profit = (currentPrice - buyPrice) * quantity;
-            if(Profit > 0){
-                System.out.println("Bạn lãi. Chúc mừng bạn đã lãi số tiền " + Profit);
-            }else if(Profit < 0){
-                System.out.println("Bạn đang lỗ "+ Profit + ". Hãy bình tĩnh gồng lỗ");
-            }else{
-                System.out.println("Bạn đang hòa vốn. Hãy kiên nhẫn thêm");
+            if(password.equals("Java2026")){
+                System.out.println("Chào mừng bạn quay trở lại");
+            }else {
+                isLocked = false;
+                System.out.println("Bạn đã nhập sai mật khẩu, hãy thử lại");
             }
-
-            System.out.print("Bạn có muốn tiếp tục không?: Y/N ? ");
-            String choice = input.nextLine();
-            if(choice.equalsIgnoreCase("n")){
-                tiepTuc = false;
-            }
-
-            input.close();
+        input.close();
         }
         }
     }
